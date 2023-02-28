57294efec225cff116071f5796984e9cc67e0302
qkransrb <qkransrb90@gmail.com>
2023-03-01 01:57:49 +0900

    modify response message

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController {
 
     @GetMapping
     public ResponseEntity<String> index() {
-        return new ResponseEntity<>("Hi, there! - v2", HttpStatus.OK);
+        return new ResponseEntity<>("Hi, there! - v3", HttpStatus.OK);
     }
 }
