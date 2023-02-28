a21a0b746fcd82d0af69ce40b568f3ccf1496184
qkransrb <qkransrb90@gmail.com>
2023-02-28 21:08:51 +0900

    initial commit

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<>("Hi, there!", HttpStatus.OK);
+    }
+}
